bd9b76206faca0249ba2b9a0f5472c611e4d4df7
Genis <genis@example.com>
2026-03-14 03:55:29 -0300

    feat: adicionar fundo de castelo e limpar cache

--- sw.js
+++ sw.js
@@ -1,16 +1,32 @@
-const CACHE_NAME = 'royal-puzzle-v1';
+const CACHE_NAME = 'royal-puzzle-v2';
 const ASSETS = [
   './',
   './index.html',
   './css/style.css',
   './assets/castle.png',
+  './assets/castle_path.png',
   'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Montserrat:wght@300;400;700&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
